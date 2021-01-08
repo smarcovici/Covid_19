95ba9ba28b74ae9721991da739d1ff90b99851b8
smarcovici <smarcovici@netcourrier.com>
2021-01-08 13:43:24 +0100

    nouvelle version

---
+++ Surveillance_deconfinement/Images/Mise_a_jour_remote_svg.cmd
@@ -0,0 +1,4 @@
+cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
+git add .
+git commit -m "nouvelle version"
+git push -u origin master
