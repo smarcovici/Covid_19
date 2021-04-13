b688732297c14d3d4f0f9d440fd459572ee31228
smarcovici <smarcovici@netcourrier.com>
2021-04-13 14:12:09 +0200

    Surveillance du 2021-04-12

--- Surveillance_deconfinement/Images/Sauvegarde_github.cmd
+++ Surveillance_deconfinement/Images/Sauvegarde_github.cmd
@@ -1,4 +1,5 @@
+set Commentaire=%1
 cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
 git add .
-git commit -m "nouvelle version"
+if not defined Commentaire (git commit -m "Sauvegarde manuelle") else (git commit -m %Commentaire%)
 git push -u origin master
